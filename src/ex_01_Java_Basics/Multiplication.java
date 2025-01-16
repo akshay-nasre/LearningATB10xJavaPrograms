2cada6a14af34bb787267b71d7d8e3608547005f
akshay-nasre <akshaynasre2010@gmail.com>
2025-01-16 09:34:21 +0530

    Added multiplication class

---
+++ src/ex_01_Java_Basics/Multiplication.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int num = 9;
+
+        for (int i = 1; i <= 10; i++) {
+            int m = num*i;
+            System.out.println(num + " × " + i + " = " + m);
+
+        }
+    }
+}
