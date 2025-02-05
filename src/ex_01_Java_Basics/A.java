ad0f53cde3707a535d131dc84f04885940e27bf5
akshay-nasre <akshaynasre2010@gmail.com>
2025-02-05 09:56:13 +0530

    Added 29 Jan Task

---
+++ src/ex_01_Java_Basics/A.java
@@ -0,0 +1,23 @@
+package ex_01_Java_Basics;
+
+public class A {
+
+
+        public static void main(String[] args) {
+
+            String a = "A";
+            String b = "B";
+            int c = 1;
+            int d = 2;
+
+            int z = 10;
+            z++;
+            System.out.println(z);
+
+            System.out.println(a+b+c+d);
+            System.out.println(c+d+a+b);
+
+
+
+    }
+}
