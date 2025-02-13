f0eeb48c114ad072ed2d03af0e5275fbc33512ea
akshay-nasre <akshaynasre2010@gmail.com>
2025-02-13 20:10:32 +0530

    Added 1 Feb Task

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("A");
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = scanner.nextInt();
+        int a[] = new int[size];
+
+        for(int i=0;i<a.length;i++){
+            System.out.println("enter the array elemetnt");
+            a[i] = scanner.nextInt();
+        }
+
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        System.out.println(Arrays.toString(a));
     }
 }
